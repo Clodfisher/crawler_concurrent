e248a15131ea4922c3a9f657b9a2e9fe1fa8f5c9
HunterYuan <hunter_yuan@outlook.com>
2018-05-03 22:35:07 +0800

    添加item存储elasticsearch中的代码

--- persist/itemsaver.go
+++ persist/itemsaver.go
@@ -1,6 +1,11 @@
 package persist
 
-import "log"
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"gopkg.in/olivere/elastic.v5"
+	"log"
+)
 
 func ItemSaver() chan interface{} {
 	out := make(chan interface{})
@@ -10,7 +15,30 @@ func ItemSaver() chan interface{} {
 			item := <-out
 			log.Printf("Item Save: got item #%d: %v", itemCount, item)
 			itemCount++
+
+			save(item)
 		}
 	}()
 	return out
+}
+
+//有两种方式，进行elasticsearch的存储，一种是http启用rest，一种是用elasticsearch客户端
+func save(item interface{}) {
+	//must sniff false in docker
+	client, err := elastic.NewClient(elastic.SetURL("http://192.168.79.133:9200/"),
+		elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Index().
+		Index("dating_profile").
+		Type("zhenai").
+		BodyJson(item).
+		Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", resp)
 }
